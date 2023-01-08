8f76de035db49a470be5bf0d8f0744f51377f641
hientran-sudo <hien.tran@student.fairfield.edu>
2023-01-08 14:56:53 -0500

    PracticeInf

---
+++ app.js
@@ -0,0 +1,7 @@
+//http
+var http = require("http");
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello World");
+});
+server.listen(80);
